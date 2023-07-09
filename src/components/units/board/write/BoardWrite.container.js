f93833c8c530ded936827195a916951711923b86
jjunohj <jjunohj@gmail.com>
2023-07-09 20:38:23 +0800

    Add: src 폴더 추가 -> 컴포넌트별 관리

---
+++ src/components/units/board/write/BoardWrite.container.js
@@ -0,0 +1,49 @@
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { CREATE_BOARD } from "./BoardWrite.queries";
+
+export default function BoardWrite() {
+  // 자바스크립트 영역
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        // variables 이게 $ 역할을 해준다. 원래는 $를 써줘야함
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    alert(result.data.createBoard.message);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  // HTML 영역
+  // props 객체 생성
+  return (
+    <BoardWriteUI
+      onClickSubmit={onClickSubmit}
+      onChangeWriter={onChangeWriter}
+      onChangeTitle={onChangeTitle}
+      onChangeContents={onChangeContents}
+    />
+  );
+}
---
+++ src/components/units/board/write/BoardWrite.styles.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const RedInput = styled.input`
+  border: 1px solid red;
+`;
+
+export const BlueButton = styled.button`
+  background-color: blue;
+  color: white;
+`;
