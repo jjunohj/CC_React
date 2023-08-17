031c689489cfef2063556c99d09a7b7e492daa61
jjunohj <jjunohj@gmail.com>
2023-08-17 23:15:54 +0900

    Refactor: 레이아웃 폴더구조 정리

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,15 +1,36 @@
+import { useRouter } from "next/router";
+import LayoutBanner from "./banner";
+import LayoutFooter from "./footer";
+import LayoutHeader from "./header";
+import LayoutNavigation from "./navigation";
+
+const HIDDEN_HEADERS = [
+  "/12-02-library-star",
+  // ... ,
+  // ... ,
+  // ...
+];
+
 interface ILayoutProps {
   children: JSX.Element;
 }
 
 export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  console.log(router.asPath);
+
+  const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
+
   return (
     <>
-      <div>여기는 헤더입니다</div>
-      <div>여기는 배너입니다</div>
-      <div>여기는 메뉴입니다</div>
-      <div>{props.children}</div>
-      <div>여기는 푸터입니다</div>
+      {!isHiddenHeader && <LayoutHeader />}
+      <LayoutBanner />
+      <LayoutNavigation />
+      <div style={{ height: "1000px", display: "flex" }}>
+        <div style={{ width: "30%", backgroundColor: "orange" }}>사이드바</div>
+        <div style={{ width: "70%" }}>{props.children}</div>
+      </div>
+      <LayoutFooter />
     </>
   );
 }
